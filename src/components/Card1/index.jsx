8db35bdb9992e47123c5538f56a8bd63e488a4f7
lucasamaraloliveira <lucas.amaral.oliveira.silva@gmail.com>
2023-01-15 17:57:52 -0300

    style: adjusted styling of cards

--- src/components/Card1/index.jsx
+++ src/components/Card1/index.jsx
@@ -1,58 +1,69 @@
-import React from 'react'
-import './Card.css'
-import Table from '../../assets/table.png'
-import Image1 from '../../assets/item.png'
-import Image2 from '../../assets/item2.png'
-import Image3 from '../../assets/item3.png'
-import Image4 from '../../assets/item4.png'
+import React from "react";
+import "./Card.css";
+import Image1 from "../../assets/item.png";
+import Image2 from "../../assets/item2.png";
+import Image3 from "../../assets/item3.png";
+import Image4 from "../../assets/item4.png";
 
 function Card() {
   return (
-    <div className='card-container'>
-    <div className='card-layout'>
-      <div className='container-card'>
-        <div className='card'>
-          <img src={Image1} alt="" />
-        </div> 
-          <div className='card-details'>
+    <div className="card-container">
+      <div className="card-layout">
+        <div className="container-card">
+          <div className="card">
+            <img src={Image1} alt="" />
+          </div>
+          <div className="card-details">
             <h2>Product title</h2>
-            <p className='card-price'>$230,00</p>
-            <a href=""><button>Comprar</button></a>
-        </div>         
-      </div>
-      <div className='container-card'>
-        <div className='card'>
-          <img src={Image2} alt="" />
-        </div> 
-          <div className='card-details'>
+            <p className="card-price">$230,00</p>
+            <a href="">
+              <div>
+                <button>Comprar</button>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div className="container-card">
+          <div className="card">
+            <img src={Image2} alt="" />
+          </div>
+          <div className="card-details">
             <h2>Product title</h2>
-            <p className='card-price'>$230,00</p>
-            <a href=""><button>Comprar</button></a>
-        </div>     
-      </div>
-      <div className='container-card'>
-        <div className='card'>
-          <img src={Image3} alt="" />
-        </div> 
-          <div className='card-details'>
+            <p className="card-price">$230,00</p>
+            <a href="">
+              <button>Comprar</button>
+            </a>
+          </div>
+        </div>
+
+        <div className="container-card">
+          <div className="card">
+            <img src={Image3} alt="" />
+          </div>
+          <div className="card-details">
             <h2>Product title</h2>
-            <p className='card-price'>$230,00</p>
-            <a href=""><button>Comprar</button></a>
-        </div>     
-      </div>
-      <div className='container-card'>
-        <div className='card'>
-          <img src={Image4} alt="" />
-        </div> 
-          <div className='card-details'>
+            <p className="card-price">$230,00</p>
+            <a href="">
+              <button>Comprar</button>
+            </a>
+          </div>
+        </div>
+        <div className="container-card">
+          <div className="card">
+            <img src={Image4} alt="" />
+          </div>
+          <div className="card-details">
             <h2>Product title</h2>
-            <p className='card-price'>$230,00</p>
-            <a href=""><button>Comprar</button></a>
-        </div>     
+            <p className="card-price">$230,00</p>
+            <a href="">
+              <button>Comprar</button>
+            </a>
+          </div>
+        </div>
       </div>
-      </div>    
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
